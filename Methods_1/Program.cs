3e00ccb20569bce118a6f773c0334493d8b9ce39
Александр Косилов <ted7007@yandex.ru>
2022-11-02 21:42:00 +1000

    create Methods examples

---
+++ Methods_1/Program.cs
@@ -0,0 +1,70 @@
+﻿Console.WriteLine("Введите размерность первой матрицы: ");
+// число столбцов в певрой равно числу строк во второй
+int[,] A = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+Random rnd = new Random();  
+for (int i = 0; i < A.GetLength(0); i++) // вносим рандомные числа
+{
+    for (int j = 0; j < A.GetLength(1); j++)
+    {
+        A[i, j] = rnd.Next(10);
+    }
+}
+Console.WriteLine("Введите размерность второй матрицы: ");
+int[,] B = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+for (int i = 0; i < B.GetLength(0); i++)        // вносим рандомные числа
+{
+    for (int j = 0; j < B.GetLength(1); j++)
+    {
+        B[i, j] = rnd.Next(10);
+    }
+}
+
+Console.WriteLine("\nМатрица A:");
+for (int i = 0; i < A.GetLength(0); i++)    // печатаем матрицу
+{
+    for (int j = 0; j < A.GetLength(1); j++)
+    {
+        Console.Write("{0} ", A[i, j]);
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nМатрица B:");
+for (int i = 0; i < B.GetLength(0); i++) // печатаем матрицу
+{
+    for (int j = 0; j < B.GetLength(1); j++)
+    {
+        Console.Write("{0} ", B[i, j]);
+    }
+    Console.WriteLine();
+}
+
+
+
+Console.WriteLine("\nМатрица C = A * B:");
+int[,] C;
+
+
+if (A.GetLength(1) != B.GetLength(0)) 
+    throw new Exception("Матрицы нельзя перемножить");
+int[,] r = new int[A.GetLength(0), B.GetLength(1)];
+for (int i = 0; i < A.GetLength(0); i++)        // Умножаем
+{
+    for (int j = 0; j < B.GetLength(1); j++)
+    {
+        for (int k = 0; k < B.GetLength(0); k++)
+        {
+            r[i, j] += A[i, k] * B[k, j];
+        }
+    }
+}
+C = r;
+
+for (int i = 0; i < C.GetLength(0); i++) // печатаем матрицу
+{
+    for (int j = 0; j < C.GetLength(1); j++)
+    {
+        Console.Write("{0} ", C[i, j]);
+    }
+    Console.WriteLine();
+}
---
+++ Methods_2/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.WriteLine("Введите размерность первой матрицы: ");
+// число столбцов в певрой равно числу строк во второй
+int[,] A = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+
+Console.WriteLine("Введите размерность второй матрицы: ");
+int[,] B = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+
+A = GetRandomArray(A.GetLength(0), A.GetLength(1));
+B = GetRandomArray(A.GetLength(0), A.GetLength(1));
+
+Console.WriteLine("\nМатрица A:");
+Print(A);
+Console.WriteLine("\nМатрица B:");
+Print(B);
+Console.WriteLine("\nМатрица C = A * B:");
+int[,] C = Multiplication(A, B);
+Print(C);
+
+int[,] GetRandomArray(int length1, int length2)
+{
+    Random rnd = new Random();
+    int[,] A = new int[length1, length2];
+
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < A.GetLength(1); j++)
+        {
+            A[i, j] = rnd.Next(10);
+        }
+    }
+
+    return A;
+}
+
+int[,] Multiplication(int[,] a, int[,] b)
+{
+    if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+    int[,] r = new int[a.GetLength(0), b.GetLength(1)];
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            for (int k = 0; k < b.GetLength(0); k++)
+            {
+                r[i, j] += a[i, k] * b[k, j];
+            }
+        }
+    }
+    return r;
+}
+
+void Print(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            Console.Write("{0} ", a[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
